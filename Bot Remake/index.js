4e70fb7a7bef10ff8ae307f96eebad1d4223b0f0
airbornandy <49731506+airbornandy@users.noreply.github.com>
2020-01-17 11:38:13 -0800

    privated bot remake until finished

--- Bot Remake/index.js
+++ Bot Remake/index.js
@@ -1,16 +1,36 @@
 const Discord = require("discord.js");
 const botConfig = require("./config.json");
 const client = new Discord.Client({disableEveryone: true});
-let prefix = config.prefix;
+let prefix = botConfig.prefix;
+const fs = require("fs")
+client.commands = new Discord.Collection();
 
-fs.readdir(".commands/", (err, files) => {
+fs.readdir("./commands/", (err, files) => {
     if (err) console.log(err);
 
-    let jsfile = files.filter
+    let jsfile = files.filter(f => f.split(".").pop() === 'js');
+    if (jsfile <= 0) {
+        console.log("Commands not found");
+        return;
+    }
 
-})
+    jsfile.forEach((f, i) => {
+        let props = require(`./commands/${f}`);
+        console.log(`${f} loaded`);
+        client.commands.set(props.help.name, props);
+    });
+});
 
-clinet.once('ready', () => {
+client.once('ready', () => {
     console.log(`${client.user.username} is online`);
 });
 
+client.on('message', async msg => {
+    if (msg.author.bot) return;
+
+    if (!msg.content.startsWith(prefix)) return;
+
+    let messageArray
+})
+
+client.login(botConfig.token)
