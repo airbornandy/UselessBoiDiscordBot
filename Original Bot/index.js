6ce63c704a1c75d84bf121ec64cfdf2251658e9a
airbornandy <49731506+airbornandy@users.noreply.github.com>
2020-02-20 17:59:49 -0800

    AHHHHHHHHHHHH

--- Original Bot/index.js
+++ Original Bot/index.js
@@ -234,7 +234,8 @@ client.on   ('message', msg => {
                 break;
             case 'play':
                 const url = args[0];
-                play(msg.member.voiceChannel, url)
+                let channel = msg.member.voiceChannel.connection
+                play(channel, url)
                 break;
             case 'help':
                 const miscHelp = new Discord.RichEmbed()
