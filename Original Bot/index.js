a9034d9c1431609ecbe8300ab643efc7f0c90f84
airbornandy <49731506+airbornandy@users.noreply.github.com>
2020-03-03 21:55:46 -0800

    ree

--- Original Bot/index.js
+++ Original Bot/index.js
@@ -24,7 +24,7 @@ client.on   ('message', msg => {
 
     if (msg.channel.type === 'dm') {
         if (msg.author.bot) return;
-        client.channels.fetch('684638934348726303').send('DM from <@' + msg.author.id + '> containing message ' + '"' + msg.content + '"');
+        client.channels.cache.get('684638934348726303').send('DM from <@' + msg.author.id + '> containing message ' + '"' + msg.content + '"');
     }
 
     if (msg.content.slice(0, 1) === prefix) {
@@ -131,7 +131,7 @@ client.on   ('message', msg => {
                 const textMessage = args.join(" ");
                 msg.delete();
                 textUser.send(textMessage);
-                client.channels.fetch('684638934348726303').send(`Message "${textMessage}" sent to <@${textUser.id}>`)
+                client.channels.cache.get('684638934348726303').send(`Message "${textMessage}" sent to <@${textUser.id}>`)
                 break;
             // case 'ducks':
             //     msg.channel.send("Duck rape is a major issue");
